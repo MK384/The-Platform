8b9d766ce3d47d91fc2015fc4b27e74c77562f83
Mohammed Khaled <94141150+MK384@users.noreply.github.com>
2025-02-04 21:36:58 +0200

    Create the detailed-course-card component files.

---
+++ The-Platform-frontend/src/app/components/detailed-course-card/detailed-course-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './detailed-course-card.component.html',
+  styleUrl: './detailed-course-card.component.css'
+})
+export class DetailedCourseCardComponent {
+
+}
