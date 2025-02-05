7781a9b04aaf207afb5725ce535659a2d10f6d46
Mohammed Khaled <94141150+MK384@users.noreply.github.com>
2025-02-05 20:57:41 +0200

    feat: implement the category card component.

--- The-Platform-frontend/src/app/app.component.ts
+++ The-Platform-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DetailedCourseCardComponent } from "./components/detailed-course-card/detailed-course-card.component";
+import { CategoryCardComponent } from "./components/category-card/category-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DetailedCourseCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
