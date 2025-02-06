a0fa7d595d4bd0c22fb32f4c02fd217907fa37d4
Mohammed Khaled <94141150+MK384@users.noreply.github.com>
2025-02-07 00:57:39 +0200

    partially implement the home page.

--- The-Platform-frontend/src/app/app.component.ts
+++ The-Platform-frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
