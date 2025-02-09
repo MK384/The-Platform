8d31df78ef01b24f002f86759fc2e9c5ee16ff02
Aya Ashreey <Ayaashry68@gmail.com>
2025-02-09 15:59:06 +0200

    add new card in selling courses section

---
+++ The-Platform-frontend/src/app/components/regular-course-card/regular-course-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regular-course-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './regular-course-card.component.html',
+  styleUrl: './regular-course-card.component.css'
+})
+export class RegularCourseCardComponent {
+
+}
